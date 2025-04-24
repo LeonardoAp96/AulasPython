141bf628eaa97f79d678809693ab7131d3841028
LeonardoAp96 <62868494+LeonardoAp96@users.noreply.github.com>
2025-04-24 19:48:40 -0300

    Update restaurante.py

--- OO/modules/restaurante.py
+++ OO/modules/restaurante.py
@@ -1,3 +1,5 @@
+from modules.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
     
@@ -6,6 +8,7 @@ class Restaurante:
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self) #insere o restaurante na lista
     
     #o 'self' não é uma palavra reservada, referencia ao proprio objeto chamado
@@ -15,8 +18,9 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
+        print(f'{'Nome'.ljust(20)} | {'Categoria'.ljust(20)} | {'avaliacao'.ljust(20)} {'ativo'.ljust(20)}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {str(restaurante.media_avaliacoes).ljust(20)} {restaurante.ativo}')
             
     @property
     def ativo(self):
@@ -25,12 +29,17 @@ class Restaurante:
     
     def alternar_estado(self):
         self._ativo = not self._ativo
-        
 
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quant_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas/quant_de_notas, 1)
+        return media
     
-restaurante_praca = Restaurante('Praca', 'gourmet')
-restaurante_praca.alternar_estado()
-restaurante_pizza = Restaurante('pizza', 'frfr')
-
-
-Restaurante.listar_restaurantes()
